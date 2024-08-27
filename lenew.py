559612e3ae5fe345335c7f561647c0aa33cafd04
lbhagavan <lbhagavan@yahoo.com>
2024-08-27 16:42:49 -0700

    Update lenew.py

--- lenew.py
+++ lenew.py
@@ -2,43 +2,28 @@ import streamlit as st
 import random
 import time
 
+from google.colab import userdata
+  open_ai_key = userdata.get('open_ai_key')
+from openai import OpenAI
+  #client = OpenAI(api_key=open_ai_key)
+  client = OpenAI(api_key=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)
+ import os
+  os.environ["OPENAI_API_KEY"] = open_ai_key
+  from selenium import webdriver
+  from selenium.webdriver.chrome.options import Options
+  from llama_index.core import VectorStoreIndex, download_loader
+  from llama_index.readers.web import WholeSiteReader
+  import chromedriver_autoinstaller
 
 # Streamed response emulator
 def response_generator(query):
-  #%pip install openai
-  from google.colab import userdata
-  open_ai_key = userdata.get('open_ai_key')
-  from openai import OpenAI
-  #client = OpenAI(api_key=open_ai_key)
-  client = OpenAI(api_key=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)
-  ##RAG
-  #!pip install llama-index --upgrade
-  #!pip install llama-index-readers-web
-  
-  import os
-  os.environ["OPENAI_API_KEY"] = open_ai_key
-  #  install chrome and selenium driver i
-  #!apt-get update
-  #!apt install chromium-chromedriver
-  #!pip install selenium
-  #Web scarpping cal fire website
-  #  install chrome and selenium driver i
-  from selenium import webdriver
-  from selenium.webdriver.chrome.options import Options
-  import chromedriver_autoinstaller
   
   chromedriver_autoinstaller.install()  # Install the compatible chromedriver automatically
-  
   options = webdriver.ChromeOptions()
   options.add_argument('--headless')  # Run Chrome in headless mode (without GUI)
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   driver = webdriver.Chrome(options=options) #initialize ChromeDriver
-  
-  
-  from llama_index.core import VectorStoreIndex, download_loader
-  from llama_index.readers.web import WholeSiteReader
-  
   
   # Initialize the scraper with a prefix URL and maximum depth
   scraper = WholeSiteReader(
@@ -46,7 +31,6 @@ def response_generator(query):
       max_depth=6,
       driver=driver # Pass the configured driver to the WholeSiteReader
   )
-  
   # Start scraping from a base URL
   documents = scraper.load_data(
       base_url="https://www.fire.ca.gov/"
@@ -54,8 +38,8 @@ def response_generator(query):
   index = VectorStoreIndex.from_documents(documents)
   query_engine = index.as_query_engine()
 
-response = query_engine.query(query)
-#print(response)
+  response = query_engine.query(query)
+  print(response)
 return response
 
 st.title("FireBot chat")
